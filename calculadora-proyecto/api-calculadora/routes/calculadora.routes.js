d139ce17202b9b233b9003da337c46b740022882
BdBas <valenciamenajuan@gmail.com>
2025-03-13 10:20:47 -0500

    calculadora 22 de febrero

--- calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
+++ calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
@@ -6,5 +6,6 @@ router
     .post('/sumar', calculadoraControllers.sumar)
     .post('/restar', calculadoraControllers.restar)
     .post('/multiplicar', calculadoraControllers.multiplicar)
+    .post('/dividir', calculadoraControllers.dividir); // Nueva ruta para la división
 
 module.exports = router;
